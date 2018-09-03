d51547ba7c705e85d3be589cf06834e4193491f1
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-03 09:18:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+
+def word_substituter (tweet)
+  
+substitutes = {
+"hello" => 'hi',
+"to" => '2', 
+"two" => '2', 
+"too" => '2', 
+"for, four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@" ,
+"and" => "&"
+}
+new_tweet = []
+tweet.each_char do |word|
+  if word == substitutes.keys
+      word =substitutes.value
+end
+
+
+# #   new_tweet.push(word)
+
+# # substitutes.each { |k, v| tweet.sub!(k, v) }
+# # substitutes.each { |k, v| tweet.sub!(k, v) }
+# # tweet
+#   #end
+  
+# substitutes.default_proc = ->(h,k) { k }
+# tweet.each_char.map { |s| tweet = s.gsub(/\w+/, substitutes) }
+# tweet.join
+
+#end
+end
